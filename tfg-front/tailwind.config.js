d16e97cf54cc8c4296a8d3258bd67f85d974c26e
Román Larrosa <romanlarrosa@gmail.com>
2022-05-26 12:22:22 +0200

    ADD and FIX linting

--- tfg-front/tailwind.config.js
+++ tfg-front/tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: [
-    "./public/**/*.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
