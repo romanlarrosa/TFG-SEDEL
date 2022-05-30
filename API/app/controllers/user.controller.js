f3254b674c46129ae054021fb45e8b542338a2e6
Román Larrosa <romanlarrosa@gmail.com>
2022-05-30 23:32:50 +0200

    ADD a route

--- API/app/controllers/user.controller.js
+++ API/app/controllers/user.controller.js
@@ -1,3 +1,20 @@
+const db = require("../models");
+
+const {user: User} = db;
+
+exports.getAllUsers = (_req, res) => {
+    User.find({ })
+        .populate("roles")
+        .exec( (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+
+                res.json(result);
+            }
+        }); 
+};
+
 exports.allAccess = (req, res) => {
     res.status(200).send("Public content");
 };
