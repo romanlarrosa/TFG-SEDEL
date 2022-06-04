36691077018efb14390148bf2524919d495d9a3e
Román Larrosa <romanlarrosa@gmail.com>
2022-06-04 14:00:10 +0200

    ADD button to register UPDATE use external spinner

--- tfg-front/src/components/UserList.jsx
+++ tfg-front/src/components/UserList.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import UserService from '../services/user.service'
 import UserCard from 'components/UserCard'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { isUnauth } from 'handlers/unauth'
+import SedelSpinner from './SedelSpinner'
 
 const UserList = () => {
   const [users, setUsers] = useState([])
@@ -39,9 +40,17 @@ const UserList = () => {
         </div>
       )}
       <div className="flex flex-row flex-wrap justify-center align-top p-20 pt-10 gap-6 text-center w-full">
-        {loading && (
-          <span className="spinner-border text-black animate-spin inline-block w-5 h-2 border-4 rounded-full border-purple-800"></span>
-        )}
+        <div className='flex flex-col gap-12 items-center'>
+          <Link
+            to={'register'}
+            className='py-2 self-end px-4 text-sm font-medium rounded-lg border-2 border-purple-900 text-purple-900 hover:bg-purple-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-purple-900 focus:text-white'
+          >
+            Nuevo Usuario
+          </Link>
+          {loading && (
+            <SedelSpinner />
+          )}
+        </div>
         {!loading &&
           users.map((user) => <UserCard key={user._id} user={user}></UserCard>)}
       </div>
