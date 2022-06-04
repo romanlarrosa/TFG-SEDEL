7af18bbd7563d35952d7fad2f3af418044e7b9d2
Román Larrosa <romanlarrosa@gmail.com>
2022-06-04 16:55:21 +0200

    CREATE voting model

---
+++ API/app/models/voting.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Voting = mongoose.model(
+    "Voting",
+    new mongoose.Schema({
+        name: String,
+        startDate: Date,
+        endDate: Date,
+        universal: Boolean,
+        electors: [{id: String,}],
+        candidates: [{name: String,}]
+    })
+);
+module.exports = Voting;
