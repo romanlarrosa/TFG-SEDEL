62614f816f687eb09b563d11cc6f8379d5aba9f6
Román Larrosa <romanlarrosa@gmail.com>
2022-06-04 13:59:43 +0200

    REFACTOR extract spinner

---
+++ tfg-front/src/components/SedelSpinner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SedelSpinner = () => {
+  return (
+    <span className="spinner-border text-black animate-spin inline-block w-5 h-2 border-4 rounded-full border-purple-800"></span>
+  )
+}
+
+export default SedelSpinner
