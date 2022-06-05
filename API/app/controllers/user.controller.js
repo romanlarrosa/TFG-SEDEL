f60e22730a1238884febe95f6447e0379dc76026
Román Larrosa <romanlarrosa@gmail.com>
2022-06-05 20:41:51 +0200

    CLEAN test calls

--- API/app/controllers/user.controller.js
+++ API/app/controllers/user.controller.js
@@ -14,19 +14,3 @@ exports.getAllUsers = (_req, res) => {
             }
         }); 
 };
-
-exports.allAccess = (req, res) => {
-    res.status(200).send("Public content");
-};
-
-exports.userBoard = (req, res) => {
-    res.status(200).send("Public content");
-};
-
-exports.adminBoard = (req, res) => {
-    res.status(200).send("Public content");
-};
-
-exports.moderatorBoard = (req, res) => {
-    res.status(200).send("Public content");
-};
