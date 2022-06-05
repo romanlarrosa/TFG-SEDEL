67d383a9cfdf786ee97c1e6b826edc766e54b196
Román Larrosa <romanlarrosa@gmail.com>
2022-06-05 18:51:41 +0200

    ADD create voting funcition

--- tfg-front/src/services/voting.service.js
+++ tfg-front/src/services/voting.service.js
@@ -6,7 +6,21 @@ const API_URL = `${REACT_APP_API_HOST}/api/`
 const getAllVotings = () => {
   return axios.get(API_URL + 'votings', { headers: authHeader() })
 }
+
+const createVoting = (name, sufragio, startDate, endDate, candidateList, electorList) => {
+  const universal = sufragio === 'universal'
+  const voting = {
+    name,
+    startDate,
+    endDate,
+    universal,
+    electors: universal ? [] : electorList.map(elector => { return { id: elector } }),
+    candidates: candidateList.map(elector => { return { name: elector } })
+  }
+  return axios.post(API_URL + 'votings', { ...voting }, { headers: authHeader() })
+}
 const VotingService = {
-  getAllVotings
+  getAllVotings,
+  createVoting
 }
 export default VotingService
