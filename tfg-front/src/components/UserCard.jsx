25d61d797ad298a8d58f0e72f3fc46a9436ef700
Román Larrosa <romanlarrosa@gmail.com>
2022-06-07 19:59:06 +0200

    prettier changes

--- tfg-front/src/components/UserCard.jsx
+++ tfg-front/src/components/UserCard.jsx
@@ -32,32 +32,39 @@ const UserCard = (props) => {
         })
     }
   }
-  return !deleted && (
-    <div
-      className="block rounded-lg shadow-lg bg-white mx-20 w-full"
-      key={user._id}
-    >
-      <div className="overflow-hidden rounded-t-lg sm:h-28 h-10 bg-purple-400"></div>
-      <div className="w-24 h-24 -mt-12 overflow-hidden border-2 border-white rounded-full ml-8 bg-white">
-        <Avatar name={user.username}></Avatar>
+  return (
+    !deleted && (
+      <div
+        className="block rounded-lg shadow-lg bg-white mx-20 w-full"
+        key={user._id}
+      >
+        <div className="overflow-hidden rounded-t-lg sm:h-28 h-10 bg-purple-400"></div>
+        <div className="w-24 h-24 -mt-12 overflow-hidden border-2 border-white rounded-full ml-8 bg-white">
+          <Avatar name={user.username}></Avatar>
+        </div>
+        <div className="p-6 text-left">
+          <h4 className="text-2xl font-semibold mb-4">{user.username}</h4>
+          <hr />
+          <p className="mt-4">{user.email}</p>
+          {user.roles.map((role) => role.name).includes('admin') && (
+            <p>Administrator</p>
+          )}
+          {!user.roles.map((role) => role.name).includes('admin') && (
+            <p>Gestor Electoral</p>
+          )}
+          <div className="flex justify-end mt-3">
+            {showDelete && (
+              <button
+                onClick={handleDelete}
+                className="py-2 px-3 bg-red-800 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-900 active:shadow-lg transition duration-150 ease-in-out disabled:bg-red-300"
+              >
+                Eliminar
+              </button>
+            )}
+          </div>
+        </div>
       </div>
-      <div className="p-6 text-left">
-        <h4 className="text-2xl font-semibold mb-4">{user.username}</h4>
-        <hr />
-        <p className="mt-4">{user.email}</p>
-        {user.roles.map((role) => role.name).includes('admin') && (<p >Administrator</p>)}
-        {!user.roles.map((role) => role.name).includes('admin') && (<p >Gestor Electoral</p>)}
-      <div className="flex justify-end mt-3">
-        {showDelete && (<button
-        onClick={handleDelete}
-        className="py-2 px-3 bg-red-800 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-900 active:shadow-lg transition duration-150 ease-in-out disabled:bg-red-300"
-
-        >
-          Eliminar
-        </button>)}
-      </div>
-      </div>
-    </div>
+    )
   )
 }
 
