009ec5154ec72509707c2cb3c87bda718e3a9037
Román Larrosa <romanlarrosa@gmail.com>
2022-06-07 19:29:06 +0200

    ADD deleteuserById CLEAN test methods

--- tfg-front/src/services/user.service.js
+++ tfg-front/src/services/user.service.js
@@ -1,28 +1,18 @@
 import axios from 'axios'
 import authHeader from './auth-header'
 const REACT_APP_API_HOST = process.env.REACT_APP_API_HOST
-const TEST_API_URL = `${REACT_APP_API_HOST}/api/test/`
 const API_URL = `${REACT_APP_API_HOST}/api/`
-const getPublicContent = () => {
-  return axios.get(TEST_API_URL + 'all')
-}
-const getUserBoard = () => {
-  return axios.get(TEST_API_URL + 'user', { headers: authHeader() })
-}
-const getModeratorBoard = () => {
-  return axios.get(TEST_API_URL + 'mod', { headers: authHeader() })
-}
-const getAdminBoard = () => {
-  return axios.get(TEST_API_URL + 'admin', { headers: authHeader() })
-}
+
 const getAllUsers = () => {
   return axios.get(API_URL + 'users', { headers: authHeader() })
 }
+
+const deleteUserById = (id) => {
+  return axios.delete(API_URL + 'users/' + id, { headers: authHeader() })
+}
+
 const UserService = {
-  getPublicContent,
-  getUserBoard,
-  getModeratorBoard,
-  getAdminBoard,
-  getAllUsers
+  getAllUsers,
+  deleteUserById
 }
 export default UserService
