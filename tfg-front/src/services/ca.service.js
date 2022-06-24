aed4b945e3dfaf8d753b4c5063b538991d324141
Román Larrosa <romanlarrosa@gmail.com>
2022-06-24 19:42:48 +0200

    CREATE ca services

--- tfg-front/src/services/ca.service.js
+++ tfg-front/src/services/ca.service.js
@@ -1,19 +1,50 @@
-const REACT_APP_CA_HOST = process.env.REACT_APP_CA_HOST
-const API_URL = `${REACT_APP_CA_HOST}/validate/`
+import axios from 'axios'
+import * as BlindSignature from 'blind-signatures'
+const API_URL = process.env.REACT_APP_CA_HOST
 
-const validate = (form, candidate) => {
-    form.target = '_blank'
-    form.setAttribute(
-      'candidate',
-      JSON.stringify({ vote: candidate })
-    )
-    console.log(form)
-    form.submit()
+async function blind(candidate, votingId) {
+  // Get keys
+  const response = await axios.get(API_URL + '/keys')
+    const n = response.data.n
+    const e = response.data.e
+
+    const { blinded, r } = BlindSignature.blind({
+      message: candidate,
+      N: n,
+      E: e
+    })
+
+    // Save R
+    localStorage.setItem('r', r + '')
+
+    return JSON.stringify({
+      voting: votingId,
+      candidate: blinded + ''
+    })
 }
 
+async function submitForm(candidate, votingId) {
+  const form = document.createElement('form')
+  const input = document.createElement('input')
+  input.value = await blind(candidate, votingId)
+  input.name = 'vote'
+  form.appendChild(input)
+
+  form.target = '_blank'
+  form.action = `${API_URL}/test/validate`
+  form.method = 'post'
+  console.log(form)
+  document.body.appendChild(form)
+  form.submit()
+}
+
+const validate = (candidate, votingId, navigation) => {
+  submitForm(candidate, votingId)
+  navigation('/confirmVoting')
+}
 
 const CAService = {
-    validate
+  validate
 }
 
 export default CAService
