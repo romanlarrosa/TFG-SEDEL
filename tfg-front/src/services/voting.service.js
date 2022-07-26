f7dac867aa256d158dc67f1fc880db7e871ef2f7
Román Larrosa <romanlarrosa@gmail.com>
2022-07-26 09:28:47 +0200

    ADD snedVote UPDATE styling ESLint

--- tfg-front/src/services/voting.service.js
+++ tfg-front/src/services/voting.service.js
@@ -3,15 +3,28 @@ import authHeader from './auth-header'
 const REACT_APP_API_HOST = process.env.REACT_APP_API_HOST
 const API_URL = `${REACT_APP_API_HOST}/api/`
 
-const buildVoting = (name, sufragio, startDate, endDate, candidateList, electorList) => {
+const buildVoting = (
+  name,
+  sufragio,
+  startDate,
+  endDate,
+  candidateList,
+  electorList
+) => {
   const universal = sufragio === 'universal'
   return {
     name,
     startDate,
     endDate,
     universal,
-    electors: universal ? [] : electorList.map(elector => { return { id: elector } }),
-    candidates: candidateList.map(elector => { return { name: elector } })
+    electors: universal
+      ? []
+      : electorList.map((elector) => {
+          return { id: elector }
+        }),
+    candidates: candidateList.map((elector) => {
+      return { name: elector }
+    })
   }
 }
 
@@ -19,22 +32,73 @@ const getAllVotings = () => {
   return axios.get(API_URL + 'votings', { headers: authHeader() })
 }
 
-const createVoting = (name, sufragio, startDate, endDate, candidateList, electorList) => {
-  const voting = buildVoting(name, sufragio, startDate, endDate, candidateList, electorList)
-  return axios.post(API_URL + 'votings', { ...voting }, { headers: authHeader() })
+const createVoting = (
+  name,
+  sufragio,
+  startDate,
+  endDate,
+  candidateList,
+  electorList
+) => {
+  const voting = buildVoting(
+    name,
+    sufragio,
+    startDate,
+    endDate,
+    candidateList,
+    electorList
+  )
+  return axios.post(
+    API_URL + 'votings',
+    { ...voting },
+    { headers: authHeader() }
+  )
 }
 
 const getVotingById = (id) => {
-  return axios.get(API_URL + 'votings/' + id, { headers: authHeader() })
+  return axios.get(API_URL + 'votings/' + id, {
+    headers: authHeader()
+  })
 }
 
-const updateVotingById = (id, name, sufragio, startDate, endDate, candidateList, electorList) => {
-  const voting = buildVoting(name, sufragio, startDate, endDate, candidateList, electorList)
-  return axios.put(API_URL + 'votings/' + id, { ...voting }, { headers: authHeader() })
+const updateVotingById = (
+  id,
+  name,
+  sufragio,
+  startDate,
+  endDate,
+  candidateList,
+  electorList
+) => {
+  const voting = buildVoting(
+    name,
+    sufragio,
+    startDate,
+    endDate,
+    candidateList,
+    electorList
+  )
+  return axios.put(
+    API_URL + 'votings/' + id,
+    { ...voting },
+    { headers: authHeader() }
+  )
 }
 
 const deleteVotingById = (id) => {
-  return axios.delete(API_URL + 'votings/' + id, { headers: authHeader() })
+  return axios.delete(API_URL + 'votings/' + id, {
+    headers: authHeader()
+  })
+}
+
+const sendVote = () => {
+  const blinding = JSON.parse(window.localStorage.getItem('blinding'))
+  const unblinded = window.localStorage.getItem('unblinded')
+  console.log('Unblinded: ' + unblinded + '')
+  return axios.post(
+    API_URL + 'votings/' + blinding.votingId + '/vote',
+    { vote: blinding.message, signed: unblinded }
+  )
 }
 
 const VotingService = {
@@ -42,6 +106,7 @@ const VotingService = {
   createVoting,
   getVotingById,
   updateVotingById,
-  deleteVotingById
+  deleteVotingById,
+  sendVote
 }
 export default VotingService
