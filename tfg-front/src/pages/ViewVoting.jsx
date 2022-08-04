2be7a354a47989759351ccdf165940b9cd214f6b
Román Larrosa <romanlarrosa@gmail.com>
2022-08-04 18:18:38 +0200

    ADD error handling

--- tfg-front/src/pages/ViewVoting.jsx
+++ tfg-front/src/pages/ViewVoting.jsx
@@ -10,6 +10,7 @@ const ViewVoting = () => {
   const { id } = useParams()
   const [loading, setLoading] = useState(true)
   const [voting, setVoting] = useState(null)
+  const [message, setMessage] = useState('')
   const navigation = useNavigate()
 
   const hasEnded = () => {
@@ -26,21 +27,34 @@ const ViewVoting = () => {
         },
         (error) => {
           setLoading(false)
-          console.log(error);
+          setMessage(
+            `Ha ocurrido un error (${error.response.status}). Compruebe el ID y vuelva a intentarlo`
+          )
         }
       )
     }
   }, [id, navigation])
 
-  return loading ? (
-    <SedelSpinner></SedelSpinner>
-  ) : (
+  return (
     <div className='flex flex-row flex-wrap justify-center align-top p-2 sm:p-20 sm:pt-10 pt-10 gap-6 text-center w-full'>
-      <VotingCard voting={voting}></VotingCard>
-      {hasEnded() && voting.escrutined && (
+      {loading ? (
+        <SedelSpinner></SedelSpinner>
+      ) : message ? (
+        <div
+          className='bg-red-100 rounded-lg py-3 px-6 text-base text-red-700 mt-3 text-center'
+          role='alert'
+        >
+          {message}
+        </div>
+      ) : (
         <>
-          <VotesAndElectors voting={voting}></VotesAndElectors>
-          <VotesGraph voting={voting}></VotesGraph>
+          <VotingCard voting={voting}></VotingCard>
+          {hasEnded() && voting.escrutined && (
+            <>
+              <VotesAndElectors voting={voting}></VotesAndElectors>
+              <VotesGraph voting={voting}></VotesGraph>
+            </>
+          )}
         </>
       )}
     </div>
